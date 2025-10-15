c91ac75e711663734c85a3810d11181ceae16b85
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-10-15 20:59:14 -0300

    toEqual

--- Aula teste de sistemas/Teste de Sistemas/sum.test.js
+++ Aula teste de sistemas/Teste de Sistemas/sum.test.js
@@ -4,3 +4,9 @@ test ("Somando dois números inteiros", () => {
     //valores de entrada - expect (esperado) - e depois o resultado (toBe)
     expect(sum(1,1)).toBe(2);
 }); //no terminal: npm run test - para rodar o nosso teste
+
+test ('atribuição de objeto', () => {
+    const data = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+});
