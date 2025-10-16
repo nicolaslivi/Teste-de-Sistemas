f36a57295e3fcb025a20323049096f60b9f601be
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-10-15 21:22:19 -0300

    aula 15/10

--- Aula teste de sistemas/Teste de Sistemas/sum.test.js
+++ Aula teste de sistemas/Teste de Sistemas/sum.test.js
@@ -18,3 +18,44 @@ test('adicionando numeros positivos nao é zero', () => {
         }
     }
 });
+
+test('nulo', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+});
+
+test('dois mais dois', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+});
+
+test('adicionando numeros de ponto flutuante', () => {
+    const value = 0.1 + 0.2;
+    //expect(value).toBe(0.3); //isso nao vai funcionar por causa de um erro de arredondamento
+    expect(value).toBeCloseTo(0.3); //isso funciona
+});
+
+test('nao existe I em team', () => {
+    expect('team').not.toMatch(/I/);
+});
+
+test('mas existe stop em Cristopher', () => {
+    expect('Cristopher').toMatch(/stop/);
+});
+
