8ad7113cacb3657ca0bd3586b06f784402f00228
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-10-16 19:24:51 -0300

    atividade 1

---
+++ Aula teste de sistemas/Atividade 1/calc.js
@@ -0,0 +1,17 @@
+function mult(a, b){
+    return a * b;
+}
+
+function div(a, b){
+    return a / b;
+}
+
+function menos(a, b){
+    return a - b;
+}
+
+function mais(a, b){
+    return a + b;
+}
+
+module.exports = sum;
---
+++ Aula teste de sistemas/Atividade 1/calc.test.js
@@ -0,0 +1,5 @@
+const sum = require('./calc'); //preciso criar a minha varíavel de teste usando o nome do arquivo a ser testado
+
+test('fazendo uma calculadora', () => {
+    
+}); 
