a48a34ca4ad50d0f0ba344a31fe330b4052506bb
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-10-22 21:48:24 -0300

    começando

--- Aula teste de sistemas/Atividade 3/trabalho.test.js
+++ Aula teste de sistemas/Atividade 3/trabalho.test.js
@@ -1,8 +1,16 @@
+const { default: expect } = require("expect");
 const podeTrabalhar = require("./trabalho");
 
 describe("Teste da função podeTrabalhar", () => {
   test("Caso de teste 1: Idade = 82", () => {
     //Criar teste para o caso de teste
+    certo = podeTrabalhar(82);
+    expect(certo).toBe('Você ultrapassou a idade de trabalho.');
+    expect(certo).toContain('ultrapassou');
+    expect(certo).not.toBe('Você pode trabalhar.');
+    expect(certo).not.toContain('ola');
+    expect(certo).toBeDefined();
+    expect(certo).toMatch(/ultrapassou a idade/);
   });
 
   test("Caso de teste 2: Idade = 25", () => {
