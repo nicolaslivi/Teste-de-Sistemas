83ffcbc9ee9c4b16c8e3a30f2636690dc926e894
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-11-05 20:34:22 -0300

    cypress

--- Aula teste de sistemas/Atividade calculadora/cypress/e2e/spec.cy.ts
+++ Aula teste de sistemas/Atividade calculadora/cypress/e2e/spec.cy.ts
@@ -1,7 +1,15 @@
 /// <reference types="cypress" />
 
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:5173/')
+  it('Teste 1 - Acessndo URL', () => {
+    cy.visit('http://localhost:5173/');
+
+    //cy.contains('nicolas');
+
+    cy.get('button[id="2"]').click();
+    cy.get('button[id="plus"]').click();
+    cy.get('button[id="4"]').click();
+    cy.get('button[id="equal"]').click();
   })
 })
+
--- Aula teste de sistemas/Atividade calculadora/src/components/ButtonsPanel.tsx
+++ Aula teste de sistemas/Atividade calculadora/src/components/ButtonsPanel.tsx
@@ -8,21 +8,21 @@ export default function ButtonsPanel({ onClick }: ButtonsPanelProps) {
   return (
     <div className="panel">
       <Button id="AC" name="AC" onClick={onClick} />
-      <Button name="/" onClick={onClick} />
-      <Button name="7" onClick={onClick} />
-      <Button name="8" onClick={onClick} />
-      <Button name="9" onClick={onClick} />
-      <Button name="x" onClick={onClick} />
-      <Button name="4" onClick={onClick} />
-      <Button name="5" onClick={onClick} />
-      <Button name="6" onClick={onClick} />
-      <Button name="-" onClick={onClick} />
-      <Button name="1" onClick={onClick} />
-      <Button name="2" onClick={onClick} />
-      <Button name="3" onClick={onClick} />
+      <Button id="/" name="/" onClick={onClick} />
+      <Button id="7" name="7" onClick={onClick} />
+      <Button id="8" name="8" onClick={onClick} />
+      <Button id="9" name="9" onClick={onClick} />
+      <Button id="x" name="x" onClick={onClick} />
+      <Button id="4" name="4" onClick={onClick} />
+      <Button id="5" name="5" onClick={onClick} />
+      <Button id="6" name="6" onClick={onClick} />
+      <Button id="-" name="-" onClick={onClick} />
+      <Button id="1" name="1" onClick={onClick} />
+      <Button id="2" name="2" onClick={onClick} />
+      <Button id="3" name="3" onClick={onClick} />
       <Button id="plus" name="+" onClick={onClick} />
-      <Button name="0" onClick={onClick} />
-      <Button name="." onClick={onClick} />
+      <Button id="0" name="0" onClick={onClick} />
+      <Button id="." name="." onClick={onClick} />
       <Button id="equal" name="=" onClick={onClick} />
     </div>
   );
