8f27dff9bbdf838f6249759b563b3e2b2e42113a
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-11-05 21:17:43 -0300

    update soma.cy.ts

--- Aula teste de sistemas/Atividade calculadora/cypress/e2e/calculadora/soma.cy.ts
+++ Aula teste de sistemas/Atividade calculadora/cypress/e2e/calculadora/soma.cy.ts
@@ -2,7 +2,7 @@
 
 describe('template spec', () => {
     it('Somando 2 + 4 = 6', () =>{
-        cy.visit('http://localhost:5173/')
+        cy.visit('http://localhost:5173/');
         
         cy.get('button[id="2"]').click();
         cy.get('button[id="plus"]').click();
@@ -10,4 +10,27 @@ describe('template spec', () => {
         cy.get('button[id="equal"]').click();
         cy.get('.display-container').should('have.text', '6');
     });
+
+    it('Somando -2 + -2 = -4', () => {
+        cy.visit('http://localhost:5173/');
+
+        cy.get('button[id="-"]').click();
+        cy.get('button[id="2"]').click();
+        cy.get('button[id="plus"]').click();
+        cy.get('button[id="-"]').click();
+        cy.get('button[id="2"]').click();
+        cy.get('button[id="equal"]').click();
+        cy.get('.display-container').should('have.text', '-4');
+    })
+
+    it('Somando -4 + 6 = 2', () => {
+        cy.visit('http://localhost:5173/');
+
+        cy.get('button[id="-"]').click();
+        cy.get('button[id="4"]').click();
+        cy.get('button[id="plus"]').click();
+        cy.get('button[id="6"]').click();
+        cy.get('button[id="equal"]').click();
+        cy.get('.display-container').should('have.text', '2');
+    })
 });
